3137762b7d38454dc92a0fae4d28020652d99827
jkk111 <kellyj58@tcd.ie>
2018-03-28 10:03:37 +0100

    Added Upload Directory Creation

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ let socket = process.env.socket;
 let multer = require('multer');
 let express = require('express');
 let app = express();
+let fs = require('fs');
 
 const STATIC_PATH = process.env.UPLOAD_CONTENT;
+
+try {
+  fs.mkdirSync(STATIC_PATH)
+} catch(e) {
+
+}
+
 let upload = multer({ dest: STATIC_PATH });
 app.use(express.static(STATIC_PATH));
 

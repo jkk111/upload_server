394eb25618bc861fa7124a9457e64b83d541ab25
jkk111 <kellyj58@tcd.ie>
2018-03-28 10:11:45 +0100

    Added ID Return Value

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let socket = process.env.socket;
+let socket = process.env.socket || 8081;
 let multer = require('multer');
 let express = require('express');
 let app = express();
 let fs = require('fs');
 
-const STATIC_PATH = process.env.UPLOAD_CONTENT;
+const STATIC_PATH = process.env.UPLOAD_CONTENT || `${__dirname}/uploads`;
 
 try {
   fs.mkdirSync(STATIC_PATH)
@@ -15,8 +15,8 @@ try {
 let upload = multer({ dest: STATIC_PATH });
 app.use(express.static(STATIC_PATH));
 
-app.post('/', upload.single('file'), () => {
-  res.send('OK');
+app.post('/', upload.single('file'), (req, res) => {
+  res.send({ id: req.file.filename });
 })
 
 app.listen(socket);

18f3d2ebde9bdf17c070e6947457f10f6f907fd3
jkk111 <kellyj58@tcd.ie>
2018-03-28 09:59:22 +0100

    Fixed app.use

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let app = express();
 
 const STATIC_PATH = process.env.UPLOAD_CONTENT;
 let upload = multer({ dest: STATIC_PATH });
-app.use(STATIC_PATH);
+app.use(express.static(STATIC_PATH));
 
 app.post('/', multer.single('file'), () => {
   res.send('OK');
